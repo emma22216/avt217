588463baabc36e0468ddf1da8918397681d9239b
Emma Dardis <emmadardis@Emmas-MacBook-Pro-3.local>
2019-11-18 16:23:19 -0500

    p3

---
+++ E17/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  console.log('Ready!');
+  $('.snow').click(function() {
+    $('.number-1').css('text-align','left');
+  });
+});
+$(document).ready(function(){
+  console.log('Ready!');
+$('.snow').click(function() {
+  $('.number-2').css('text-align','right');
+  });
+});
+$(document).ready(function(){
+  console.log('Ready!');
+  $('.snow').click(function() {
+    $('.number-3').css('text-align','left');
+  });
+});
+$(document).ready(function(){
+  console.log('Ready!');
+$('.snow').click(function() {
+  $('.number-4').css('text-align','right');
+  });
+});
+$(document).ready(function(){
+  console.log('Ready!');
+  $('.snow').click(function() {
+    $('.number-5').css('text-align','left');
+  });
+});
