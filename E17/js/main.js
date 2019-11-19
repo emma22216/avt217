ea1ed55a5f3c92d3d3c5c005ef049b796726cdd4
Emma Dardis <emmadardis@Emmas-MacBook-Pro-3.local>
2019-11-19 12:55:11 -0500

    e17

--- E17/js/main.js
+++ E17/js/main.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
   console.log('Ready!');
+$('.snow').click(function() {
+  $('.number-1').fadeOut( "slow", function() {
+  });
+});
+$('.snow').hover(function() {
+  $('.number-1').fadeIn( "slow", function() {
+  });
+});
+$('.snow').click(function(){
+ $('.number-5').effect( "bounce", {times:3}, 300 );
+  });
+});
+$('.snow').click(function() {
+  $('.number-4').animate({
+    width: "130%",
+    fontSize: "11em",
+    borderWidth: "40px"
+  }, 1500 );
+});
+$('.snow').click(function() {
+  $('.number-4').animate({
+    width: "100%",
+    fontSize: "9em",
+    borderWidth: "40px"
+  }, 1500 );
+});
+$(document).ready(function(){
+  console.log('Ready!');
   $('.snow').click(function() {
     $('.number-1').css('text-align','left');
-  });
-});
-$(document).ready(function(){
-  console.log('Ready!');
-$('.snow').click(function() {
-  $('.number-2').css('text-align','right');
-  });
-});
-$(document).ready(function(){
-  console.log('Ready!');
-  $('.snow').click(function() {
-    $('.number-3').css('text-align','left');
-  });
-});
-$(document).ready(function(){
-  console.log('Ready!');
-$('.snow').click(function() {
-  $('.number-4').css('text-align','right');
-  });
-});
-$(document).ready(function(){
-  console.log('Ready!');
-  $('.snow').click(function() {
-    $('.number-5').css('text-align','left');
   });
 });
