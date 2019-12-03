a4aa10b60632a3dd0f237a2c9c44e018bfa176da
Emma Dardis <emmadardis@Emmas-MacBook-Pro-3.local>
2019-12-03 18:26:33 -0500

    p5

--- P5/js/color.js
+++ P5/js/color.js
@@ -11,29 +11,31 @@ $(document).ready(function(){
 	var moves = 0;
 	var secs = 0;
 
+
 	//  shuffle the tiles
   function shuffleTiles(){
 		if(shuffle == 1){
-			var input = $('input:image').css({top: 340, left: 680});
+			var input = $('#pic').css({top: 0, left: 340});
 		} else if(shuffle == 2){
-			var input = $('input:image').css({top: 340, left: 0});
+			var input = $('#pic').css({top: 340, left: 0});
 		} else if(shuffle == 3){
-			var input = $('input:image').css({top: 0, left: 680});
+			var input = $('#pic').css({top: 0, left: 680});
 		} else if(shuffle == 4){
-			var input = $('input:image').css({top: 0, left: 680});
-
-		}
-	}
-  $(document).ready(function() {
+			var input = $('#pic').css({top: 340, left: 340});
+    } else if(shuffle == 5){
+      var input = $('#pic').css({top: 680, left: 0});
+  }
+  }
+  function start(){
    setInterval(function(){
   			shuffleTiles();
   			setInterval(function(){
   				secs++
   			}, 1000);
   		}, 1000);
-  	});
+  	}
     //  play the game
-
+    start();
   	$('.pieces').click(function(){
 
   		if(tileClicked == false){  //  if no tile is clicked
@@ -47,7 +49,7 @@ $(document).ready(function(){
   			$(this).addClass('glow');
   			tileClicked = true;
 
-  		} else{  //  if you've clicked a tile
+  		} else {  //  if you've clicked a tile
 
   			//  set variables
   			secondTileClicked = $(this).attr('id');
@@ -63,18 +65,19 @@ $(document).ready(function(){
   			tileClicked = false;
 
   			//  test for the win
-
+        var pT = parseInt($('#pic').css('top'));
+        var pL = parseInt($('#pic').css('left'));
   			setTimeout(function(){
   				if(
-
-            $('#piece-6').css('left') == '0px' && $('#piece-6').css('top') == '0px'
+            $('#piece-6').css('left') == pL + 'px' && $('#piece-6').css('top') == pT + 'px'
   				){
+            $('h1').text('You Found the Potato!')
   					$('p').text('You have solved the puzzle in ' + secs + ' seconds using ' + moves + ' moves!!');
   					$('article').addClass('glow-2');
   					moves = 0;
   				}
   			}, 1000);
-  
+
   			//  increment the move counter
   			moves++
 
